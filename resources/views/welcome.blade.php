35a7612b8dd32cd546af2a3fb9a5765fd029c5d4
emadjoha <emad1.2@hotmail.com>
2018-12-07 20:19:20 +0200

    add item and showing in users interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,21 @@
 
 
 @section('content')
-    <h1 class="text-center" style="color: red" >
+    <div class="alert alert-danger" role="alert">
         @if(\Illuminate\Support\Facades\Auth::check())
-            Welcome {{ \Illuminate\Support\Facades\Auth::user()->name    }}
-            <span style="color: blue;" >
-                ({{ \App\Language::find(session()->get('lang'))->name  }})
-            </span>
+            Welcome
+        <span class="alert-link">
+            {{ \Illuminate\Support\Facades\Auth::user()->name    }}
+        </span>
+    </div>
+            <img src="{{asset('../resources/pics/traffic-sign.png')}}" alt="Lights" style="padding-left:25%">
             {{--{{phpinfo()}}--}}
         @else
             You are logged out
         @endif
+
+
+    <h1 class="text-center" style="color: red" >
+
     </h1>
 @endsection
